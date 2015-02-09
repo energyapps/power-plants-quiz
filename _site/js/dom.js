f8e06ddc7d4d3db86b3503d619916356834b6085
Daniel Wood <daniel.j.wood11@gmail.com>
2015-02-09 17:32:54 -0500

    dynamically add questions from json

---
+++ _site/js/dom.js
@@ -0,0 +1,13 @@
+'<div class="rowDOE full-size-blocks" ><div class="large-12 columnsDOE sub-blocks-full map-image"><div class="inner-text q-number">' + i +
+'</div></div></div><div class="rowDOE"><div class="large-12 medium-12 small-12"><div class="question subheadline"><p>' +
+data[i].question +
+'</p></div><div class="rowDOE"><div class="small-12 small-centered columnsDOE"><div class="rowDOE answers_block"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
+data[i].answer +
+'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
+data[i].ans2 +
+'</p></div></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
+data[i].ans3 +
+'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
+data[i].ans4 +
+'</p></div></div></div></div></div></div></div></div><div class="large-12 tweener"></div></div>'
+
---
+++ js/data/fake.js
@@ -0,0 +1,18 @@
+var data = [
+  {
+    "question":"What is the capital of this state?",
+    "answer":"Harrisburg",
+    "ans2":"Pittsburgh",
+    "ans3":"Mechanicsburg",
+    "ans4":"Gettysburg",
+    "img":"http://placehold.it/745x345"
+  },
+  {
+    "question":"Who is the true fifth Beatle?",
+    "answer":"Billy Preston",
+    "ans2":"Stuart Sutcliffe",
+    "ans3":"Pete Best",
+    "ans4":"Tony Sheridan",
+    "img":"http://placehold.it/745x345"
+  }
+];
