05ddbee195cfc69ce18232e0c21f17aff59a9e35
Daniel Wood <daniel.j.wood11@gmail.com>
2015-02-10 16:45:07 -0500

    add clicking functionality

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,3 +1,5 @@
+// some variables
+var NumOfQuestions = data.length;
 
 // Shuffle Function
 function shuffle(o){ //v1.0
@@ -16,20 +18,57 @@ for (var i = 0; i < data.length; i++) {
 	shuffle(raw)
 
 	// set content to have answers in them
-	var content = '<div class="rowDOE full-size-blocks" ><div class="large-12 columnsDOE sub-blocks-full map-image"><div class="inner-text q-number">' + (i + 1) +
+	var content = '<div class="question-individual"><div class="rowDOE full-size-blocks" ><div class="large-12 columnsDOE sub-blocks-full map-image"><div class="inner-text q-number">' + (i + 1) +
 	'.</div></div></div><div class="rowDOE"><div class="large-12 medium-12 small-12"><div class="question subheadline"><p>' +
 	data[i].question +
-	'</p></div><div class="rowDOE"><div class="small-12 small-centered columnsDOE"><div class="rowDOE answers_block"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE '+ raw[0].correct +'"><p>' +
+	'</p></div><div class="rowDOE"><div class="small-12 small-centered columnsDOE"><div class="rowDOE answers_block"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[0].correct + '" data-id=' + i + '><p>' +
 	raw[0].ans +
-	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE '+ raw[1].correct +'"><p>' +
+	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[1].correct + '" data-id=' + i + '><p>' +
 	raw[1].ans +
-	'</p></div></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE '+ raw[2].correct +'"><p>' +
+	'</p></div></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[2].correct + '" data-id=' + i + '><p>' +
 	raw[2].ans +
-	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE '+ raw[3].correct +'"><p>' +
+	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[3].correct + '" data-id=' + i + '><p>' +
 	raw[3].ans +
-	'</p></div></div></div></div></div></div></div></div><div class="large-12 tweener"></div></div>'
+	'</p></div></div></div></div></div></div></div></div><div class="large-12 tweener"></div></div></div>'
 
 	//add content with shuffled answers to the DOM
+			// Can also change image, bind addtiontal context here.
 	$( "#questions-container" ).append( $(content) );
 };
+
+
+
+//////-----------------////////
+// Colors based on correct incorrect clicks. Only allow one click per question
+// on click do the following
+// index += 1 so that after 1 click, no more clicks
+// if index == 0 change color else do nothing
+// if index == 0 change the final number
+// when index of all indexes equals total number of questions  
+
+// array of 0's and iteratate through based on order of questions in the dom.....
+var QuestionIndex = [];
+
+for (var k = 0; k < NumOfQuestions; k++) {
+	QuestionIndex.push(0);
+};
+
+//clicking the first time per question causes a question to be answered. after that it does nothing. (see if statement inside)
+$('.a-bg').click(function (e) {
+	var current_q = $(this).attr("data-id")
+
+	if (QuestionIndex[current_q] === 0) {
+		//should be first click
+		$(this).addClass('active');
+		QuestionIndex[current_q]+=1;
+	};
+});
+
+
+
+// on each click of button, change total correct/incorrect number....use that as a trigger
+
+
+
+
 
