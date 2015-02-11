fb5f21229909e49c87ca69ea0cffc7c7c1ee15e7
Daniel Wood <daniel.j.wood11@gmail.com>
2015-02-11 13:39:47 -0500

    update margins on answer buttons

--- _site/js/dom.js
+++ _site/js/dom.js
@@ -1,13 +1,12 @@
-'<div class="rowDOE full-size-blocks" ><div class="large-12 columnsDOE sub-blocks-full map-image"><div class="inner-text q-number">' + i +
-'</div></div></div><div class="rowDOE"><div class="large-12 medium-12 small-12"><div class="question subheadline"><p>' +
-data[i].question +
-'</p></div><div class="rowDOE"><div class="small-12 small-centered columnsDOE"><div class="rowDOE answers_block"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
-data[i].answer +
-'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
-data[i].ans2 +
-'</p></div></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
-data[i].ans3 +
-'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered a-bg columnsDOE"><p>' +
-data[i].ans4 +
-'</p></div></div></div></div></div></div></div></div><div class="large-12 tweener"></div></div>'
-
+var content = '<div data-id="' + i + '" id="question' + (i+1) + '" class="question-individual"><div class="rowDOE full-size-blocks" ><div class="large-12 columnsDOE sub-blocks-full map-image"><div class="inner-text q-number">' + (i + 1) +
+	'.</div></div></div><div class="rowDOE"><div class="large-12 medium-12 small-12"><div class="question subheadline"><p>' +
+	data[i].question +
+	'</p></div><div class="rowDOE"><div class="small-12 small-centered columnsDOE"><div class="rowDOE answers_block"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[0].correct + '" data-id=' + i + '><p>' +
+	raw[0].ans +
+	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[1].correct + '" data-id=' + i + '><p>' +
+	raw[1].ans +
+	'</p></div></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[2].correct + '" data-id=' + i + '><p>' +
+	raw[2].ans +
+	'</p></div></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="rowDOE"><div class="large-11 medium-11 small-11 small-centered columnsDOE a-bg q' + (i+1) + ' ' + raw[3].correct + '" data-id=' + i + '><p>' +
+	raw[3].ans +
+	'</p></div></div></div></div></div></div></div></div><div class="large-12 tweener"></div></div></div>'
