8c6b3cc43536c430575931126cb152135e8256c3
Daniel Wood <daniel.j.wood11@gmail.com>
2015-03-03 12:03:21 -0500

    add content

---
+++ _site/js/data/quizquestions_web.js
@@ -0,0 +1,113 @@
+var data =
+[
+  {
+    "id":1,
+    "question":"This is the leading electricity source in the U.S.",
+    "ans1":"Natural Gas",
+    "ans2":"Petroleum Products",
+    "ans3":"Coal",
+    "ans4":"Nuclear Power",
+    "img":"/sites/prod/files/ng.png",
+    "cont":"Natural gas leads the nation in total capacity, with almost 500 gigawatts from 1,551 plants. "
+  },
+  {
+    "id":2,
+    "question":"Found primarily in the South, we may need \"proof\" that this fuel source is consumed appropriately.",
+    "ans1":"Black Liquor",
+    "ans2":"Nuclear Power",
+    "ans3":"Landfill Gas",
+    "ans4":"Sludge Waste",
+    "img":"/sites/prod/files/blq.png",
+    "cont":"\nBlack liquor is a by-product of paper production. Many mills burn it  as a primary source of their electricity. As a result, the paper pulp industry is one of our country's more carbon-neutral industries."
+  },
+  {
+    "id":3,
+    "question":"This fossil fuel is used all over the Midwest, Hawaii and Alaska",
+    "ans1":"Petroleum Products",
+    "ans2":"Coal",
+    "ans3":"Blast Furnace Gas",
+    "ans4":"Kerosene",
+    "img":"/sites/prod/files/petroleum.png",
+    "cont":"Petroleum has the fourth highest installed capacity, after natural gas, coal and hydroelectricity. Why so low? Though we use more petroleum than natural gas overall, much of this usage is in our cars and trucks."
+  },
+  {
+    "id":4,
+    "question":"You can find this fuel source in some awfully sunny places.",
+    "ans1":"Geothermal Power",
+    "ans2":"Jet Fuel",
+    "ans3":"Hydroelectric Power",
+    "ans4":"Solar Power",
+    "img":"/sites/prod/files/geo.png",
+    "cont":"Fooled you! While it happens that geothermal power resources are located in some sunny, warm places, all of these locations have geothermal activity near the surface."
+  },
+  {
+    "id":5,
+    "question":"What's going on in Western Alaska?",
+    "ans1":"Jet Fuel",
+    "ans2":"Wind Power",
+    "ans3":"Sludge Waste",
+    "ans4":"Agricultural By-Products",
+    "img":"/sites/prod/files/jf.png",
+    "cont":"We know: you keep hoping that the answer is sludge waste, but it's not. Jet fuel can be used for electricity generation, mainly in power plants that are only operated during times of peak demand due to how expensive it is to run them."
+  },
+  {
+    "id":6,
+    "question":"This fuel source is captured as a by-product of another large scale process.",
+    "ans1":"Landfill Gas",
+    "ans2":"Sludge Waste",
+    "ans3":"Black Liquor",
+    "ans4":"Agricultural By-Products",
+    "img":"/sites/prod/files/lfg.png",
+    "cont":"Landfill gas is methane that is captured at landfill sites, refined and burned to create electricity. Capturing methane has grown in popularity in recent years, as it is a far more potent greenhouse gas than carbon dioxide -- its by-product when burned."
+  },
+  {
+    "id":7,
+    "question":"Very few of these power plants exist on the West Coast, but they tend to have very high generation capacity per power plant",
+    "ans1":"Nuclear Power",
+    "ans2":"Natural Gas",
+    "ans3":"Coal",
+    "ans4":"Wood Waste Liquids",
+    "img":"/sites/prod/files/nuc.png",
+    "cont":"The most recent new nuclear power plant came online in 1996."
+  },
+  {
+    "id":8,
+    "question":"Which fuel source seems like a it stretches all the way down the Appalachian Mountains?",
+    "ans1":"Coal",
+    "ans2":"Wind Power",
+    "ans3":"Solar Power",
+    "ans4":"Hydroelectric Power",
+    "img":"/sites/prod/files/coal.png",
+    "cont":""
+  },
+  {
+    "id":9,
+    "question":"Seems like Massachusetts, New Jersey and North Carolina are all in love with this power source, but don't let that fact deceive you.",
+    "ans1":"Solar Power",
+    "ans2":"Wood Waste Liquids",
+    "ans3":"Hydroelectric Power",
+    "ans4":"Wind Power",
+    "img":"/sites/prod/files/sun_2.png",
+    "cont":"Though it's growing rapidly on the East Coast as installation costs continue to plummet, utility-scale solar power is still strongest in the Southwest, where sunny days are plentiful and hot."
+  },
+  {
+    "id":10,
+    "question":"This is the leading renewable energy source in capacity and number of generators.",
+    "ans1":"Hydroelectric Power",
+    "ans2":"Geothermal Power",
+    "ans3":"Agricultural By-Products",
+    "ans4":"Wind Power",
+    "img":"/sites/prod/files/wat.png",
+    "cont":"The oldest continually operated electricity generator in the country is a hydroelectric facility in Stevens Point, Wisconsin. It came online in 1891."
+  },
+  {
+    "id":11,
+    "question":"This fuel source has grown by leaps and bounds in the last few years, particularly in Iowa, Texas and Oklahoma.",
+    "ans1":"Wind Power",
+    "ans2":"Petroleum Products",
+    "ans3":"Sludge Waste",
+    "ans4":"Subbituminous Coal",
+    "img":"/sites/prod/files/wnd.png",
+    "cont":"We're blown away by wind power's growth recently, though in the Southern U.S. there isn't much wind, and therefore not much development."
+  }
+];
